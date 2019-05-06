c7252b12e9e7035800e9e9b627ef058b4b80905a
Aruna Benjamin <arunabenji29@gmail.com>
2019-05-06 17:29:38 -0600

    created a new component

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import SavedList from './SavedList';
+import MovieCard from './MovieCard'
 
 export default class Movie extends Component {
   constructor(props) {
@@ -28,27 +29,29 @@ export default class Movie extends Component {
         console.error(error);
       });
   };
-  // Uncomment this code when you're ready for the stretch problems
-  // componentWillReceiveProps(newProps){
-  //   if(this.props.match.params.id !== newProps.match.params.id){
-  //     this.fetchMovie(newProps.match.params.id);
-  //   }
-  // }
 
-  // saveMovie = () => {
-  //   const addToSavedList = this.props.addToSavedList;
-  //   addToSavedList(this.state.movie)
-  // }
+  // Uncomment this code when you're ready for the stretch problems
+  componentWillReceiveProps(newProps){
+    if(this.props.match.params.id !== newProps.match.params.id){
+      this.fetchMovie(newProps.match.params.id);
+    }
+  }
+
+  saveMovie = () => {
+    const addToSavedList = this.props.addToSavedList;
+    addToSavedList(this.state.movie)
+  }
 
   render() {
     if (!this.state.movie) {
       return <div>Loading movie information...</div>;
     }
 
-    const { title, director, metascore, stars } = this.state.movie;
+    // const { title, director, metascore, stars } = this.state.movie;
     return (
       <div className="save-wrapper">
-        <div className="movie-card">
+      <MovieCard movie={this.state.movie} />
+        {/* <div className="movie-card">
           <h2>{title}</h2>
           <div className="movie-director">
             Director: <em>{director}</em>
@@ -63,7 +66,7 @@ export default class Movie extends Component {
               {star}
             </div>
           ))}
-        </div>
+        </div> */}
         <div className="save-button">Save</div>
       </div>
     );
--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import {Link} from 'react-router-dom'
+import MovieCard from './MovieCard'
+
 export default class MovieList extends Component {
   constructor(props) {
     super(props);
@@ -25,7 +27,8 @@ export default class MovieList extends Component {
       <div className="movie-list">
         {this.state.movies.map(movie => (
           <Link to={`/movies/${movie.id}`}>
-          <MovieDetails key={movie.id} movie={movie} />
+          {/* <MovieDetails key={movie.id} movie={movie} /> */}
+          <MovieCard key={movie.id} movie={movie} />
           </Link>
         ))}
       </div>
@@ -33,29 +36,29 @@ export default class MovieList extends Component {
   }
 }
 
-function MovieDetails({ movie }) {
-  const { title, director, metascore, stars } = movie;
-  return (
+// function MovieDetails({ movie }) {
+//   const { title, director, metascore, stars } = movie;
+//   return (
     
-    <div className="movie-card">
+//     <div className="movie-card">
       
-        <h2>{title}</h2>
-        <div className="movie-director">
-          Director: <em>{director}</em>
-        </div>
-        <div className="movie-metascore">
-          Metascore: <strong>{metascore}</strong>
-        </div>
-        <h3>Actors</h3>
+//         <h2>{title}</h2>
+//         <div className="movie-director">
+//           Director: <em>{director}</em>
+//         </div>
+//         <div className="movie-metascore">
+//           Metascore: <strong>{metascore}</strong>
+//         </div>
+//         <h3>Actors</h3>
 
-        {stars.map(star => (
-          <div key={star} className="movie-star">
-            {star}
-          </div>
-        ))}
-        {/* </Link> */}
-        </div>
+//         {stars.map(star => (
+//           <div key={star} className="movie-star">
+//             {star}
+//           </div>
+//         ))}
+//         {/* </Link> */}
+//         </div>
       
     
-  );
-}
+//   );
+// }
