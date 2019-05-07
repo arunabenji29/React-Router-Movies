838140c2bba2f4ce8b5a6308d269274b9443c55f
Aruna Benjamin <arunabenji29@gmail.com>
2019-05-07 14:33:27 -0600

    converted Links to NavLinks

--- client/src/Movies/SavedList.js
+++ client/src/Movies/SavedList.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import {Route, Link} from 'react-router-dom';
+import {Route, NavLink,Link} from 'react-router-dom';
 // import MovieList from './MovieList';
 
 export default class SavedList extends Component {
@@ -19,9 +19,9 @@ export default class SavedList extends Component {
       <div className="saved-list">
         <h3>Saved Movies:</h3>
         {this.props.list.map(movie => (
-          <Link to={`/movies/${movie.id}`}>
+          <NavLink to={`/movies/${movie.id}`}>
           <span className="saved-movie">{movie.title}</span>
-          </Link>
+          </NavLink>
         ))}
         
         <Link className="home-button" to='../'>Home</Link>
